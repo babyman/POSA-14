d4417e4a91727c73ca710c88bc2775cb1784578c
Evan Jehu <evan.jehu@gmail.com>
2014-06-22 15:01:22 -0400

    week 6 code, first cut

--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
@@ -2,6 +2,8 @@ package edu.vuum.mocca;
 
 import java.lang.ref.WeakReference;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
@@ -15,14 +17,14 @@ import android.widget.Toast;
  * downloading the image, it stores it on the Android file system,
  * then notifies this activity using the Messenger IPC mechanism
  * discussed in the class.
- * 
+ *
  * Starting services to run synchronously from the asynchronous UI
  * Thread is an example of the Half-Sync/Half-Async Pattern.  Starting
  * services using Intents is an example of the Command Processor
  * Pattern. This activity, the Creator, creates a Command in the form
  * of an Intent. The Intent is received by the service process, which
  * plays the role of the Executor.
- * 
+ *
  * Returning a result using Messages and Handlers is an example of the
  * Active Object Pattern. The Service must invoke a method to update
  * the UI. However, the service thread is not allowed to interact with
@@ -31,7 +33,7 @@ import android.widget.Toast;
  * role of Active Object. The message is then passed to the UI
  * thread's handler, which eventually executes the request on the UI
  * Thread.
- * 
+ *
  * Note: all UI functionality has been factored out into
  * DownloadBase. If you wish to display an image, use
  * displayBitmap(). If you want to get the URL from the EditText
@@ -45,32 +47,32 @@ public class DownloadActivity extends DownloadBase {
      *
      * The handler plays several roles in the Active Object pattern,
      * including Proxy, Future, and Servant.
-     * 
+     *
      * Please use displayBitmap() defined in DownloadBase
      */
     static class MessengerHandler extends Handler {
-	    
+
     	// A weak reference to the enclosing class
     	WeakReference<DownloadActivity> outerClass;
-    	
+
     	/**
     	 * A constructor that gets a weak reference to the enclosing class.
     	 * We do this to avoid memory leaks during Java Garbage Collection.
-    	 * 
+    	 *
     	 * @see https://groups.google.com/forum/#!msg/android-developers/1aPZXZG6kWk/lIYDavGYn5UJ
     	 */
     	public MessengerHandler(DownloadActivity outer) {
             outerClass = new WeakReference<DownloadActivity>(outer);
     	}
-    	
+
     	// Handle any messages that get sent to this Handler
     	@Override
 		public void handleMessage(Message msg) {
-    		
+
             // Get an actual reference to the DownloadActivity
             // from the WeakReference.
             final DownloadActivity activity = outerClass.get();
-    		
+
             // If DownloadActivity hasn't been garbage collected
             // (closed by user), display the sent image.
             if (activity != null) {
@@ -78,6 +80,11 @@ public class DownloadActivity extends DownloadBase {
                 // bitmap that's been downloaded and returned to
                 // the DownloadActivity as a pathname who's Bundle
             	// key is defined by DownloadUtils.PATHNAME_KEY
+
+              Bundle bundle = msg.getData();
+              String pathname = bundle.getString(DownloadUtils.PATHNAME_KEY);
+              activity.displayBitmap(pathname);
+
             }
     	}
     }
@@ -87,15 +94,15 @@ public class DownloadActivity extends DownloadBase {
      * DownloadActivity to be stored as a WeakReference
      */
     MessengerHandler handler = new MessengerHandler(this);
-    
+
     /**
      * This method is called when a user presses a button (see
      * res/layout/activity_download.xml)
-     * 
+     *
      * Start a service using startService() or make a call to a Bound
      * Service using an AIDL interface.  The action performed depends
      * on the button pressed.
-     * 
+     *
      * To get the URL from the EditText, please use getUrlString()
      * defined in DownloadBase.
      */
@@ -108,17 +115,25 @@ public class DownloadActivity extends DownloadBase {
             // DownloadIntentService with the appropriate Intent
             // returned from the makeIntent() factory method.
 
-            which = "Starting IntentService";
+          getApplicationContext().startService(
+              DownloadIntentService.makeIntent(getApplicationContext(), handler, getUrlString())
+          );
+
+          which = "Starting IntentService";
             break;
-        
+
         case R.id.thread_pool_button:
             // TODO - You fill in here to start the
             // ThreadPoolDownloadService with the appropriate Intent
             // returned from the makeIntent() factory method.
 
+            getApplicationContext().startService(
+                ThreadPoolDownloadService.makeIntent(getApplicationContext(), handler, getUrlString())
+            );
+
             which = "Starting ThreadPoolDownloadService";
             break;
-        
+
         }
 
     	// Display a short pop-up notification telling the user which
--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/ThreadPoolDownloadService.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/ThreadPoolDownloadService.java
@@ -17,13 +17,13 @@ import android.os.Messenger;
  *        within a pool of Threads.  When it is created, it creates a
  *        ThreadPoolExecutor using the newFixedThreadPool() method of
  *        the Executors class.
- * 
+ *
  *        When this Service is started, it should be supplied with a
  *        URI for download and a Messenger.  It downloads the URI
  *        supplied, stores it on the Android file system, then returns
  *        the pathname of the downloaded file using the supplied
  *        Messenger.
- * 
+ *
  *        This class implements the Synchronous Service layer of the
  *        Half-Sync/Half-Async pattern.  It also implements a variant
  *        of the Factory Method pattern.
@@ -34,7 +34,7 @@ public class ThreadPoolDownloadService extends Service {
      * used to service download requests.
      */
     static final int MAX_THREADS = 4;
-	
+
     /**
      * The ExecutorService that references a ThreadPool.
      */
@@ -49,18 +49,18 @@ public class ThreadPoolDownloadService extends Service {
         // FixedThreadPool Executor that's configured to use
         // MAX_THREADS. Use a factory method in the Executors class.
 
-        mExecutor = null;
+        mExecutor = Executors.newFixedThreadPool(MAX_THREADS);
     }
 
     /**
      * Make an intent that will start this service if supplied to
      * startService() as a parameter.
-     * 
+     *
      * @param context		The context of the calling component.
      * @param handler		The handler that the service should
-     *                          use to respond with a result  
+     *                          use to respond with a result
      * @param uri               The web URL of a file to download
-     * 
+     *
      * This method utilizes the Factory Method makeMessengerIntent()
      * from the DownloadUtils class.  The returned intent is a Command
      * in the Command Processor Pattern. The intent contains a
@@ -74,7 +74,7 @@ public class ThreadPoolDownloadService extends Service {
         // invocation of the appropriate factory method in
         // DownloadUtils that makes a MessengerIntent.
 
-        return null;
+      return DownloadUtils.makeMessengerIntent(context, ThreadPoolDownloadService.class, handler, uri);
     }
 
     /**
@@ -93,10 +93,16 @@ public class ThreadPoolDownloadService extends Service {
         // the uri in the intent and returns the file's pathname using
         // a Messenger who's Bundle key is defined by DownloadUtils.MESSENGER_KEY.
 
-        Runnable downloadRunnable = null;
+        Runnable downloadRunnable = new Runnable() {
+          @Override
+          public void run() {
+            Messenger messenger = intent.getParcelableExtra(DownloadUtils.MESSENGER_KEY);
+            DownloadUtils.downloadAndRespond(getApplicationContext(), intent.getData(), messenger);
+          }
+        };
 
         mExecutor.execute(downloadRunnable);
-      
+
         // Tell the Android framework how to behave if this service is
         // interrupted.  In our case, we want to restart the service
         // then re-deliver the intent so that all files are eventually
